25a5d15de46a641a8f38217b8f930bbb6a1a277d
matthewjohnstone96 <31934326+matthewjohnstone96@users.noreply.github.com>
2017-09-13 13:09:51 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cout << "Enter the coefficients a, b, and c (# # #): ";
+    cin >> a >> b >> c;
+    cout << endl << a << "x^2  + " << b "x + " << c " = 0" << endl;
+    
+    
+  
+}
